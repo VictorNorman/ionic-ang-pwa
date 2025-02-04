0863c2110f5eff5e299c8f036ff3afcaa829f5d2
VictorNorman <vtn2@calvin.edu>
2025-02-04 13:48:49 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-pwa',
+  webDir: 'www'
+};
+
+export default config;
